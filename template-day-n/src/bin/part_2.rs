e08137f5b455f61050f654b3cb1921d0023e1c99
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-02 08:26:18 +0000

    init repo

---
+++ template-day-n/src/bin/part_2.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let input = include_str!("../../../inputs/day-N.txt");
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    fn test_1() {
+        assert!(true)
+    }
+}
