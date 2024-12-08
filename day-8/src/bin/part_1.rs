b9c79b728cd9b1345d9dccf93c41edc297528ebe
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-08 06:41:04 +0000

    finish day 8 part 1

--- day-8/src/bin/part_1.rs
+++ day-8/src/bin/part_1.rs
@@ -4,5 +4,5 @@ fn main() {
     let res = day_8::part_1::process(input);
 
     println!("res: {res}");
-    assert_eq!(res, 999);
+    assert_eq!(res, 379);
 }
--- day-8/src/part_1.rs
+++ day-8/src/part_1.rs
@@ -1,22 +1,41 @@
+use crate::{find_antennas, find_antinodes, parse_input};
+
 pub fn process(input: &str) -> usize {
-    todo!()
+    let matrix = parse_input(input);
+    let antennas = find_antennas(&matrix);
+    let antinodes = find_antinodes(&matrix, &antennas);
+
+    antinodes.len()
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
+    use rstest::{fixture, rstest};
 
     use super::*;
 
-    #[rstest]
-    fn test_process() {
-        // given
-        let input = "";
+    #[fixture]
+    fn input() -> &'static str {
+        "......#....#
+...#....0...
+....#0....#.
+..#....0....
+....0....#..
+.#....A.....
+...#........
+#......#....
+........A...
+.........A..
+..........#.
+..........#."
+    }
 
+    #[rstest]
+    fn test_process(input: &str) {
         // when
         let output = process(input);
 
         // then
-        assert_eq!(output, todo!())
+        assert_eq!(output, 14)
     }
 }
