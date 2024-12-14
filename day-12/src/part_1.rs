320fd6fa3d4543645047bcee378b6dabfdcdebb9
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-14 12:37:33 +0000

    finish day 12 part 1

--- day-12/src/part_1.rs
+++ day-12/src/part_1.rs
@@ -1,5 +1,123 @@
+use std::collections::{HashMap, HashSet};
+
+type Grid = Vec<Vec<char>>;
+type Coords = (usize, usize);
+type Regions = HashMap<usize, Region>;
+
+const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+#[derive(Debug)]
+struct Region {
+    points: HashSet<Coords>,
+}
+
+impl Region {
+    fn new() -> Self {
+        Self {
+            points: HashSet::new(),
+        }
+    }
+
+    fn area(&self) -> usize {
+        self.points.len()
+    }
+
+    fn perimeter(&self) -> usize {
+        let mut perimeter = 0;
+
+        for (i, j) in self.points.iter() {
+            let mut cell_perimeter = 4;
+
+            for (di, dj) in DIRECTIONS {
+                let next_i = *i as i32 + di;
+                let next_j = *j as i32 + dj;
+
+                if self.points.contains(&(next_i as usize, next_j as usize)) {
+                    cell_perimeter -= 1;
+                }
+            }
+
+            perimeter += cell_perimeter
+        }
+
+        perimeter
+    }
+
+    fn price(&self) -> usize {
+        self.area() * self.perimeter()
+    }
+}
+
+fn find_region(
+    i: usize,
+    j: usize,
+    grid: &Grid,
+    visited: &mut HashSet<Coords>,
+    region: &mut Region,
+) {
+    if visited.contains(&(i, j)) {
+        return;
+    }
+
+    visited.insert((i, j));
+    region.points.insert((i, j));
+
+    for (di, dj) in DIRECTIONS {
+        let next_i = i as i32 + di;
+        let next_j = j as i32 + dj;
+
+        if next_i < 0
+            || next_i as usize >= grid.len()
+            || next_j < 0
+            || next_j as usize >= grid[0].len()
+        {
+            continue;
+        }
+
+        // we know it's positive number by now
+        let next_i = next_i as usize;
+        let next_j = next_j as usize;
+
+        if grid[i][j] != grid[next_i][next_j] {
+            continue;
+        }
+
+        find_region(next_i, next_j, grid, visited, region);
+    }
+}
+
 pub fn process(input: &str) -> usize {
-    todo!()
+    let grid: Grid = input
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    let mut res = 0;
+
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut region_id: usize = 0;
+    let mut regions: Regions = HashMap::new();
+
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if visited.contains(&(i, j)) {
+                continue;
+            }
+            let mut region = Region::new();
+
+            find_region(i, j, &grid, &mut visited, &mut region);
+
+            regions.entry(region_id).or_insert(region);
+            region_id += 1;
+        }
+    }
+
+    for (_, r) in regions {
+        res += r.price()
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -10,7 +128,16 @@ mod tests {
 
     #[fixture]
     fn input() -> &'static str {
-        ""
+        "RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"
     }
 
     #[rstest]
@@ -19,6 +146,6 @@ mod tests {
         let output = process(input);
 
         // then
-        assert_eq!(output, todo!())
+        assert_eq!(output, 1930)
     }
 }
