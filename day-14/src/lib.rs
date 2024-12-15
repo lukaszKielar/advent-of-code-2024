f4923f7d9e8d5505cb67bdcec6236c1f72a01ecd
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-15 11:25:43 +0000

    day 14 part 1 attempt

--- day-14/src/lib.rs
+++ day-14/src/lib.rs
@@ -1,2 +1,175 @@
+use regex::Regex;
+
 pub mod part_1;
 pub mod part_2;
+
+const X: usize = 101;
+const Y: usize = 103;
+
+#[derive(Debug, PartialEq)]
+struct Robot {
+    p: (usize, usize),
+    v: (isize, isize),
+}
+
+impl Robot {
+    fn move_sec(&mut self, mut sec: u32, grid: &(usize, usize)) {
+        while sec > 0 {
+            let mut new_px = self.p.0 as isize + self.v.0;
+
+            if new_px < 0 {
+                new_px = grid.0 as isize + new_px;
+            } else if new_px >= grid.0 as isize {
+                new_px = new_px - grid.0 as isize;
+            }
+
+            let mut new_py = self.p.1 as isize + self.v.1;
+
+            if new_py < 0 {
+                new_py = grid.1 as isize + new_py;
+            } else if new_py >= grid.1 as isize {
+                new_py = grid.1 as isize - new_py;
+            }
+
+            self.p = (new_px as usize, new_py as usize);
+
+            sec -= 1;
+        }
+    }
+}
+
+impl From<&str> for Robot {
+    fn from(value: &str) -> Self {
+        let re =
+            Regex::new(r"^p=(?<px>\d+),(?<py>\d+) v=(?<vx>-{0,1}\d+),(?<vy>-{0,1}\d+)$").unwrap();
+        let caps = re.captures(value).unwrap();
+
+        Self {
+            p: (caps["px"].parse().unwrap(), caps["py"].parse().unwrap()),
+            v: (caps["vx"].parse().unwrap(), caps["vy"].parse().unwrap()),
+        }
+    }
+}
+
+fn parse_input(input: &str) -> Vec<Robot> {
+    input.trim().lines().map(|line| Robot::from(line)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::{fixture, rstest};
+
+    use super::*;
+
+    #[fixture]
+    fn input() -> &'static str {
+        "p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"
+    }
+
+    #[rstest]
+    fn test_robot_from_str() {
+        // given
+        let input = "p=52,66 v=37,34";
+
+        // when
+        let output = Robot::from(input);
+
+        // then
+        assert_eq!(
+            output,
+            Robot {
+                p: (52, 66),
+                v: (37, 34)
+            }
+        )
+    }
+
+    #[rstest]
+    #[case(1, (4,1))]
+    #[case(2, (6,5))]
+    #[case(3, (8,2))]
+    #[case(4, (10,6))]
+    #[case(5, (1,3))]
+    fn test_robot_move_sec(#[case] sec: u32, #[case] expected: (usize, usize)) {
+        // given
+        let mut robot = Robot::from("p=2,4 v=2,-3");
+
+        // when
+        robot.move_sec(sec, &(11, 7));
+
+        // then
+        assert_eq!(robot.p, expected);
+    }
+
+    #[rstest]
+    fn test_parse_input(input: &str) {
+        // when
+        let output = parse_input(input);
+
+        // then
+        assert_eq!(
+            output,
+            vec![
+                Robot {
+                    p: (0, 4),
+                    v: (3, -3)
+                },
+                Robot {
+                    p: (6, 3),
+                    v: (-1, -3)
+                },
+                Robot {
+                    p: (10, 3),
+                    v: (-1, 2)
+                },
+                Robot {
+                    p: (2, 0),
+                    v: (2, -1)
+                },
+                Robot {
+                    p: (0, 0),
+                    v: (1, 3)
+                },
+                Robot {
+                    p: (3, 0),
+                    v: (-2, -2)
+                },
+                Robot {
+                    p: (7, 6),
+                    v: (-1, -3)
+                },
+                Robot {
+                    p: (3, 0),
+                    v: (-1, -2)
+                },
+                Robot {
+                    p: (9, 3),
+                    v: (2, 3)
+                },
+                Robot {
+                    p: (7, 3),
+                    v: (-1, 2)
+                },
+                Robot {
+                    p: (2, 4),
+                    v: (2, -3)
+                },
+                Robot {
+                    p: (9, 5),
+                    v: (-3, -3)
+                },
+            ]
+        )
+    }
+}
--- day-14/src/part_1.rs
+++ day-14/src/part_1.rs
@@ -1,5 +1,42 @@
+use std::collections::{HashMap, HashSet};
+
+use crate::{parse_input, X, Y};
+
 pub fn process(input: &str) -> usize {
-    todo!()
+    let robots = parse_input(input);
+
+    let mut map: HashMap<u8, usize> = HashMap::new();
+    map.entry(1).or_insert(0);
+    map.entry(2).or_insert(0);
+    map.entry(3).or_insert(0);
+    map.entry(4).or_insert(0);
+
+    for mut robot in robots {
+        robot.move_sec(100, &(X, Y));
+
+        let ending_position = robot.p;
+
+        if (ending_position.0 >= 0 && ending_position.0 <= X / 2)
+            && (ending_position.1 >= 0 && ending_position.1 <= Y / 2)
+        {
+            map.entry(1).and_modify(|count| *count += 1);
+        } else if (ending_position.0 > X / 2 && ending_position.0 <= X)
+            && (ending_position.1 >= 0 && ending_position.1 <= Y / 2)
+        {
+            map.entry(2).and_modify(|count| *count += 1);
+        } else if (ending_position.0 >= 0 && ending_position.0 <= X / 2)
+            && (ending_position.1 > Y / 2 && ending_position.1 <= Y)
+        {
+            map.entry(3).and_modify(|count| *count += 1);
+        } else {
+            map.entry(4).and_modify(|count| *count += 1);
+        }
+    }
+
+    println!("{map:?}");
+
+    let res = map.values().fold(1, |acc, &x| acc * x);
+    res
 }
 
 #[cfg(test)]
@@ -16,6 +53,7 @@ mod tests {
     #[rstest]
     fn test_process(input: &str) {
         // when
+        assert_eq!(X / 2, 2);
         let output = process(input);
 
         // then
