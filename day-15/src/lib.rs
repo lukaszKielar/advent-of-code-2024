1c369156627d2ab5cffbb94d68e6674cd171cfa8
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-16 10:36:21 +0000

    start implementing day 15

--- day-15/src/lib.rs
+++ day-15/src/lib.rs
@@ -1,2 +1,110 @@
 pub mod part_1;
 pub mod part_2;
+
+type Grid = Vec<Vec<char>>;
+type Moves = Vec<(i32, i32)>;
+
+fn parse_input(input: &str) -> (Grid, Moves) {
+    let input = input.trim().split("\n\n").collect::<Vec<_>>();
+
+    let grid = input[0]
+        .trim()
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+
+    println!("{grid:?}");
+
+    let moves = input[1]
+        .trim()
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|char| match char {
+                    '>' => (0, 1),
+                    '^' => (-1, 0),
+                    '<' => (0, -1),
+                    'v' => (1, 0),
+                    _ => panic!("it won't happen"),
+                })
+                .collect::<Vec<_>>()
+        })
+        .flatten()
+        .collect();
+
+    (grid, moves)
+}
+
+fn get_robot_position(grid: &Grid) -> (usize, usize) {
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == '@' {
+                return (i, j);
+            }
+        }
+    }
+    panic!("it won't happen")
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::{fixture, rstest};
+
+    use super::*;
+
+    #[fixture]
+    fn input() -> &'static str {
+        "########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<"
+    }
+
+    #[rstest]
+    fn test_parse_input() {
+        // input
+        let input = "####
+#..#
+#0.#
+#00#
+#@.#
+####
+
+>^<v
+>^<v";
+
+        // when
+        let output = parse_input(input);
+
+        // then
+        assert_eq!(
+            output,
+            (
+                vec![
+                    vec!['#', '#', '#', '#'],
+                    vec!['#', '.', '.', '#'],
+                    vec!['#', '0', '.', '#'],
+                    vec!['#', '0', '0', '#'],
+                    vec!['#', '@', '.', '#'],
+                    vec!['#', '#', '#', '#'],
+                ],
+                vec![
+                    (0, 1),
+                    (-1, 0),
+                    (0, -1),
+                    (1, 0),
+                    (0, 1),
+                    (-1, 0),
+                    (0, -1),
+                    (1, 0),
+                ]
+            )
+        )
+    }
+}
--- day-15/src/part_1.rs
+++ day-15/src/part_1.rs
@@ -1,5 +1,37 @@
+use crate::{get_robot_position, parse_input};
+
 pub fn process(input: &str) -> usize {
-    todo!()
+    let (mut grid, moves) = parse_input(input);
+    let mut robot = get_robot_position(&grid);
+
+    for m in moves {
+        println!(
+            "before move: {:?}, robot [i={:?},j={:?}]",
+            m, robot.0, robot.1
+        );
+        let ni = robot.0 as i32 + m.0;
+        let nj = robot.1 as i32 + m.1;
+
+        let nchar = grid[ni as usize][nj as usize];
+
+        if nchar == '#' {
+            continue;
+        }
+
+        if nchar == '.' {
+            grid[robot.0][robot.1] = '.';
+            robot = (ni as usize, nj as usize);
+            grid[robot.0][robot.1] = '@';
+            continue;
+        }
+
+        if nchar == 'O' {
+            // find all connected boxes in line, and move entire section a distance
+            todo!()
+        }
+    }
+
+    0
 }
 
 #[cfg(test)]
@@ -10,7 +42,16 @@ mod tests {
 
     #[fixture]
     fn input() -> &'static str {
-        ""
+        "########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<"
     }
 
     #[rstest]
@@ -19,6 +60,6 @@ mod tests {
         let output = process(input);
 
         // then
-        assert_eq!(output, todo!())
+        assert_eq!(output, 2028)
     }
 }
