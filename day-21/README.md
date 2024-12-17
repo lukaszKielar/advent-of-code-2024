5f774a2e602108fa95563bf5f38c71626ab66d62
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-17 06:28:45 +0000

    prepare for day 21

---
+++ day-21/README.md
@@ -0,0 +1,5 @@
+# Day 21: <TITLE>
+
+## Part 1
+
+## Part 2
---
+++ day-21/src/bin/part_2.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input = include_str!("../../../inputs/day-21.txt");
+
+    let res = day_21::part_2::process(input);
+
+    println!("res: {res}");
+    assert_eq!(res, 999);
+}
