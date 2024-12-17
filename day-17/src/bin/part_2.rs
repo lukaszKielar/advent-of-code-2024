007c138a1ca599d356aa0218024eeb04bb9fd71b
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-17 06:25:01 +0000

    prepare for day 17

---
+++ day-17/src/bin/part_2.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let input = include_str!("../../../inputs/day-17.txt");
+
+    let res = day_17::part_2::process(input);
+
+    println!("res: {res}");
+    assert_eq!(res, 999);
+}
