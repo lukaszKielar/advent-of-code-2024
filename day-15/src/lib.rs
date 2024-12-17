0586419a93eb5f122ea8c28f733f50dbcc556ee7
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-17 09:25:38 +0000

    finish day 15 part 1

--- day-15/src/lib.rs
+++ day-15/src/lib.rs
@@ -2,7 +2,22 @@ pub mod part_1;
 pub mod part_2;
 
 type Grid = Vec<Vec<char>>;
-type Moves = Vec<(i32, i32)>;
+type Moves = Vec<Point>;
+
+#[derive(Debug, PartialEq)]
+struct Point {
+    i: i32,
+    j: i32,
+}
+
+impl From<(i32, i32)> for Point {
+    fn from(value: (i32, i32)) -> Self {
+        Self {
+            i: value.0,
+            j: value.1,
+        }
+    }
+}
 
 fn parse_input(input: &str) -> (Grid, Moves) {
     let input = input.trim().split("\n\n").collect::<Vec<_>>();
@@ -21,10 +36,10 @@ fn parse_input(input: &str) -> (Grid, Moves) {
         .map(|line| {
             line.chars()
                 .map(|char| match char {
-                    '>' => (0, 1),
-                    '^' => (-1, 0),
-                    '<' => (0, -1),
-                    'v' => (1, 0),
+                    '>' => (0, 1).into(),
+                    '^' => (-1, 0).into(),
+                    '<' => (0, -1).into(),
+                    'v' => (1, 0).into(),
                     _ => panic!("it won't happen"),
                 })
                 .collect::<Vec<_>>()
@@ -35,11 +50,11 @@ fn parse_input(input: &str) -> (Grid, Moves) {
     (grid, moves)
 }
 
-fn get_robot_position(grid: &Grid) -> (usize, usize) {
+fn get_robot_position(grid: &Grid) -> Point {
     for i in 0..grid.len() {
         for j in 0..grid[0].len() {
             if grid[i][j] == '@' {
-                return (i, j);
+                return (i as i32, j as i32).into();
             }
         }
     }
@@ -95,14 +110,14 @@ mod tests {
                     vec!['#', '#', '#', '#'],
                 ],
                 vec![
-                    (0, 1),
-                    (-1, 0),
-                    (0, -1),
-                    (1, 0),
-                    (0, 1),
-                    (-1, 0),
-                    (0, -1),
-                    (1, 0),
+                    (0, 1).into(),
+                    (-1, 0).into(),
+                    (0, -1).into(),
+                    (1, 0).into(),
+                    (0, 1).into(),
+                    (-1, 0).into(),
+                    (0, -1).into(),
+                    (1, 0).into(),
                 ]
             )
         )
--- day-15/src/part_1.rs
+++ day-15/src/part_1.rs
@@ -1,37 +1,53 @@
-use crate::{get_robot_position, parse_input};
+use crate::{get_robot_position, parse_input, Point};
 
 pub fn process(input: &str) -> usize {
     let (mut grid, moves) = parse_input(input);
     let mut robot = get_robot_position(&grid);
+    let mut res = 0;
 
     for m in moves {
+        let next_position: Point = (robot.i + m.i, robot.j + m.j).into();
+        let next_char = grid[next_position.i as usize][next_position.j as usize];
         println!(
-            "before move: {:?}, robot [i={:?},j={:?}]",
-            m, robot.0, robot.1
+            "robot [x={:?},y={:?}], move: {:?}, next_char: [{:?}]",
+            robot.i, robot.j, m, next_char
         );
-        let ni = robot.0 as i32 + m.0;
-        let nj = robot.1 as i32 + m.1;
 
-        let nchar = grid[ni as usize][nj as usize];
-
-        if nchar == '#' {
+        if grid[next_position.i as usize][next_position.j as usize] == '#' {
             continue;
-        }
+        } else if grid[next_position.i as usize][next_position.j as usize] == '.' {
+            grid[robot.i as usize][robot.j as usize] = '.';
+            robot = next_position;
+            grid[robot.i as usize][robot.j as usize] = '@';
+        } else if grid[next_position.i as usize][next_position.j as usize] == 'O' {
+            let mut next_next_position: Point =
+                (next_position.i + m.i, next_position.j + m.j).into();
 
-        if nchar == '.' {
-            grid[robot.0][robot.1] = '.';
-            robot = (ni as usize, nj as usize);
-            grid[robot.0][robot.1] = '@';
-            continue;
-        }
+            while grid[next_next_position.i as usize][next_next_position.j as usize] == 'O' {
+                next_next_position =
+                    (next_next_position.i + m.i, next_next_position.j + m.j).into();
+            }
 
-        if nchar == 'O' {
-            // find all connected boxes in line, and move entire section a distance
-            todo!()
+            if grid[next_next_position.i as usize][next_next_position.j as usize] == '#' {
+                continue;
+            } else if grid[next_next_position.i as usize][next_next_position.j as usize] == '.' {
+                grid[next_next_position.i as usize][next_next_position.j as usize] = 'O';
+                grid[robot.i as usize][robot.j as usize] = '.';
+                robot = next_position;
+                grid[robot.i as usize][robot.j as usize] = '@';
+            }
         }
     }
 
-    0
+    for i in 0..grid.len() {
+        for j in 0..grid[0].len() {
+            if grid[i][j] == 'O' {
+                res += 100 * i + j
+            }
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
