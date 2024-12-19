5f7f1dbc80844a6e01f341ba24cbcca16d999377
Lukasz Kielar <31779738+lukaszKielar@users.noreply.github.com>
2024-12-19 09:07:39 +0000

    attempt

--- day-19/src/part_1.rs
+++ day-19/src/part_1.rs
@@ -1,5 +1,35 @@
+use crate::parse_input;
+
 pub fn process(input: &str) -> usize {
-    todo!()
+    let (patterns, designs) = parse_input(input);
+    let mut res = 0;
+
+    for design in designs {
+        println!("{design:?}");
+        let mut design = design.to_owned();
+        let mut chars = 0;
+
+        while !design.is_empty() {
+            println!("\t{design:?}, {chars}");
+            let ref pat = &design[..design.len() - chars];
+            if patterns.contains(pat) {
+                design = design[design.len() - chars..].to_owned();
+                chars = 0;
+            } else {
+                if chars >= design.len() {
+                    break;
+                }
+                chars += 1;
+            }
+        }
+
+        if design.is_empty() {
+            println!("\tdone");
+            res += 1;
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -10,7 +40,16 @@ mod tests {
 
     #[fixture]
     fn input() -> &'static str {
-        ""
+        "r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"
     }
 
     #[rstest]
@@ -19,6 +58,6 @@ mod tests {
         let output = process(input);
 
         // then
-        assert_eq!(output, todo!())
+        assert_eq!(output, 6)
     }
 }
